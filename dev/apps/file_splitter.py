fea9f4ddd5e3958b21b3c1a92f9e414b0338703f
itversity <dgadiraju@itversity.com>
2021-11-25 06:55:00 -0600

    added orders by date data set

---
+++ dev/apps/file_splitter.py
@@ -0,0 +1,22 @@
+import os
+import pandas as pd
+
+
+source_base_dir = '/Users/itversity/Projects/Internal/bootcamp/itversity-material/mastering-airflow/data/retail_db'
+data_set_dir = 'orders'
+target_base_dir = '/Users/itversity/Projects/Internal/bootcamp/itversity-material/mastering-airflow/data/retail_db'
+target_data_set_dir = 'orders_by_date'
+
+os.makedirs(f'{target_base_dir}/{target_data_set_dir}', exist_ok=True)
+
+data_set_dir_path = f'{source_base_dir}/{data_set_dir}'
+for file_name in os.listdir(data_set_dir_path):
+    df = pd.read_csv(
+        f'{data_set_dir_path}/{file_name}',
+        names=['order_id', 'order_date', 'order_customer_id', 'order_status']
+    )
+    df_grouped = df.groupby('order_date')
+
+    for file_name, data_to_be_saved in map(lambda dfg: (dfg[0], dfg[1]), df_grouped):
+        data_to_be_saved.to_csv(f'{target_base_dir}/{target_data_set_dir}/part-{file_name[:10]}')
+        print(f'Data for date {file_name} is successfully saved')
